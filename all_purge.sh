d6abbc6ee6bb92022379419255c0db57a57ef958
Christian Zufferey <christian@zufferey.com>
2019-07-12 10:51:12 +0200

    first commit, mais cela ne marche pas encore ;-(

---
+++ all_purge.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Petit script pour nettoyer tout le binz
+#ATTENTION, cela purge VRAIMENT tout le binz de Docker !
+#zf190712.1050
+
+./list.sh
+read -p "Etes-vous certain de vouloir tout effacer ?"
+read -p "Mais cela va VRAIMENT VRAIMENT tout effacer!"
+
+docker container rm -f -v $(docker container ls -a -q)
+docker image rm -f $(docker image ls -a -q)
+docker volume rm $(docker volume ls -q)
+docker network rm $(docker network ls -q)
+
+sudo rm -rf ./db_data
+
