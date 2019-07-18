bcf9e8c05169d1fb82a37296c0d947f0fae8125c
Christian Zufferey <christian@zufferey.com>
2019-07-18 10:25:10 +0200

    Y'avait un petit nain dans le système, maintenant cela fonctionne :-)

--- purge.sh
+++ purge.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Petit script pour nettoyer tout le binz du docker-compose
-#zf190718.0921
+#zf190718.1021
 
-docker-compose down -v --remove-orphans
 sudo umount -f ./nas_share
+docker-compose down -v --remove-orphans
 sudo rm -rf ./db_data
 sudo rm -rf ./nfs_share
 sudo rm -rf ./nas_share
