ec11444ee42f7e4a8450ee74095f2767b2fec44f
Christian Zufferey <christian@zufferey.com>
2019-07-18 09:35:35 +0200

    J'ai tout cassé ça marche plus :-(

--- purge.sh
+++ purge.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Petit script pour nettoyer tout le binz du docker-compose
-#zf190712.1227
+#zf190718.0921
 
 docker-compose down -v --remove-orphans
-sudo umount ./nas_share
+sudo umount -f ./nas_share
 sudo rm -rf ./db_data
 sudo rm -rf ./nfs_share
 sudo rm -rf ./nas_share
--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #Petit script pour démarrer tout le binz
-#zf190718.0859
+#zf190718.0907
 
 #pour installer Docker et Docker compose sur une machine Ubuntu c'est ici:
 #https://github.com/zuzu59/docker_demo
 
 #docker network create frontend
 
-mkdir db_data
-mkdir nfs_share/op
-mkdir nfs_share/wp/wp1
-mkdir nfs_share/wp/wp2
-mkdir nas_share
+mkdir -p db_data
+mkdir -p nfs_share/op
+mkdir -p nfs_share/wp/wp1
+mkdir -p nfs_share/wp/wp2
+mkdir -p nas_share
 
 #docker-compose up
 docker-compose up -d
@@ -30,7 +30,7 @@ docker-compose ps
 
 pour 'entrer' dans un 'service':
 docker-compose exec nom_service /bin/bash
-docker-compose exec wordpress0 /bin/bash
+docker-compose exec nas /bin/bash
 docker-compose exec db /bin/bash
 
 pour arrêter:
